369954f260f6edd14fe817f9c02260b9be100987
manhdt711 <dtm.it2002@gmail.com>
2024-05-21 23:19:24 +0700

    22/5/2024DTO va xu li voi date kieu long Them lop

--- src/main/java/fpt/CapstoneSU24/controller/UserController.java
+++ src/main/java/fpt/CapstoneSU24/controller/UserController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/user")
@@ -29,31 +30,49 @@ public class UserController {
 
     @GetMapping("/getAllUser")
     public ResponseEntity getAllUser() {
-      List<User> userList = userRepository.findAll();
+        List<User> userList = userRepository.findAll();
         return ResponseEntity.ok(userList);
     }
 
     @GetMapping("/findByEmail")
-    public Page<User> getUsersByEmail(@RequestParam(value = "email", required = false) String email,
-                                      @RequestParam(required = false) Integer roleId,
-                                      @RequestParam(required = false) Integer status,
-                                      @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateFrom,
-                                      @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateTo,
-                                      @RequestParam(required = false, defaultValue = "createOn") String orderBy,
-                                      @RequestParam(required = false, defaultValue = "false") Boolean isAsc,
-                                      @RequestParam(required = false, defaultValue = "0") int page,
-                                      @RequestParam(required = false, defaultValue = "10") int size) {
+    public ResponseEntity<List<B03_GetDataGridDTO>> getUsersByEmail(@RequestParam(value = "email", required = false) String email,
+                                                                    @RequestParam(required = false) Integer roleId,
+                                                                    @RequestParam(required = false) Integer status,
+                                                                    @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateFrom,
+                                                                    @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateTo,
+                                                                    @RequestParam(required = false, defaultValue = "createOn") String orderBy,
+                                                                    @RequestParam(required = false, defaultValue = "false") Boolean isAsc,
+                                                                    @RequestParam(required = false, defaultValue = "0") int page,
+                                                                    @RequestParam(required = false, defaultValue = "10") int size) {
 
         Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, orderBy);
 
-        // Chuyển đổi Date thành timestamp kiểu long
-        Long timestampFrom = dateFrom != null ? dateFrom.getTime() : null;
-        Long timestampTo = dateTo != null ? dateTo.getTime() : null;
+        Long timestampFrom = dateFrom != null ? dateFrom.getTime() / 1000 : null;
+        Long timestampTo = dateTo != null ? dateTo.getTime() / 1000 : null;
 
         Pageable pageable = PageRequest.of(page, size, sort);
+        Page<User> userPage = userRepository.findByFilters(email, roleId, status, timestampFrom, timestampTo, pageable);
 
-        return userRepository.findByFilters(email, roleId, status, timestampFrom, timestampTo, pageable);
+        List<B03_GetDataGridDTO> B03_GetDataGridDTOs = userPage.getContent().stream().map(user -> {
+            B03_GetDataGridDTO B03_GetDataGridDTO = new B03_GetDataGridDTO();
+            B03_GetDataGridDTO.setUserId(user.getUserId());
+            B03_GetDataGridDTO.setEmail(user.getEmail());
+            B03_GetDataGridDTO.setRoleId(user.getRole().getRoleId());
+            B03_GetDataGridDTO.setRoleName(user.getRole().getRoleName());
+            B03_GetDataGridDTO.setName(user.getFirstName() + " " + user.getLastName());
+            B03_GetDataGridDTO.setDescription(user.getDescription());
+            B03_GetDataGridDTO.setAddress(user.getAddress());
+            B03_GetDataGridDTO.setCountry(user.getCountry());
+            B03_GetDataGridDTO.setPhone(user.getPhone());
+            B03_GetDataGridDTO.setDateOfBirth(new Date(user.getDateOfBirth() * 1000L));
+            B03_GetDataGridDTO.setStatus(user.getStatus());
+            B03_GetDataGridDTO.setCreateOn(new Date(user.getCreateOn() * 1000L));
+            B03_GetDataGridDTO.setUsername(user.getUsername());
+            return B03_GetDataGridDTO;
+        }).collect(Collectors.toList());
+
+        return ResponseEntity.ok(B03_GetDataGridDTOs);
+
     }
-
 }
