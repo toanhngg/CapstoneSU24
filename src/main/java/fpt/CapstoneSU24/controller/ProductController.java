818f3ebfeaa8aa0fcb7f12161113001830499d15
thanhnc94 <thanhnc942@gmail.com>
2024-06-21 23:54:08 +0700

    fix code

--- src/main/java/fpt/CapstoneSU24/controller/ProductController.java
+++ src/main/java/fpt/CapstoneSU24/controller/ProductController.java
@@ -2,6 +2,7 @@ package fpt.CapstoneSU24.controller;
 
 import fpt.CapstoneSU24.model.*;
 import fpt.CapstoneSU24.payload.AddProductRequest;
+import fpt.CapstoneSU24.payload.EditProductRequest;
 import fpt.CapstoneSU24.payload.FilterSearchRequest;
 import fpt.CapstoneSU24.payload.IdRequest;
 import fpt.CapstoneSU24.repository.*;
@@ -46,7 +47,7 @@ public class ProductController {
     public ResponseEntity addProduct(@Valid @RequestBody AddProductRequest req) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User currentUser = (User) authentication.getPrincipal();
-        if(currentUser.getUserId() == 2){
+        if(currentUser.getRole().getRoleId() == 2){
             try {
                 Product product = new Product();
                 product.setProductName(req.getProductName());
@@ -79,6 +80,53 @@ public class ProductController {
             return ResponseEntity.status(404).body("your account is not allowed for this action");
         }
     }
+
+
+//    - có thêm trường product Id
+//    - nếu không ko edit image (images, avatar) thì để là ""
+    @PostMapping("/editProduct")
+    public ResponseEntity editProduct(@Valid @RequestBody EditProductRequest req) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        if(currentUser.getRole().getRoleId() == 2){
+            try {
+                Product product = productRepository.findOneByProductId(req.getProductId());
+                product.setProductName(req.getProductName());
+                product.setCategory(categoryRepository.findOneByCategoryId(req.getCategoryId()));
+//            product.setUnitPrice(req.getUnitPrice());
+                product.setDimensions(req.getDimensions());
+                product.setMaterial(req.getMaterial());
+                product.setWeight(req.getWeight());
+                product.setDescription(req.getDescription());
+                product.setWarranty(req.getWarranty());
+                product.setCreateAt(System.currentTimeMillis());
+                product.setManufacturer(currentUser);
+//                product.setCertificate(certificateRepository.findOneByCertificateId(req.getCertificateId()));
+                productRepository.save(product);
+                //save image
+                if(!req.getImages().isEmpty()){
+                    for (String obj : req.getImages()) {
+                        String filePath = cloudinaryService.uploadImageAndGetPublicId(cloudinaryService.convertBase64ToImgFile(obj),"");
+                        imageProductRepository.save(new ImageProduct(0,filePath, product));
+                    }
+                }
+                //save ava
+                if(!req.getAvatar().isEmpty()){
+                    String filePathAvatar = cloudinaryService.uploadImageAndGetPublicId(cloudinaryService.convertBase64ToImgFile(req.getAvatar()), "avatar/"+product.getProductId());
+                    imageProductRepository.save(new ImageProduct(0,filePathAvatar, product));
+                }
+
+                //            return ResponseEntity.status(200).body(new String(bytes, StandardCharsets.UTF_8));
+                return ResponseEntity.status(200).body("successfully");
+            }catch (Exception e){
+                return ResponseEntity.status(500).body("error edit new product");
+            }
+            //add product
+        }else {
+            return ResponseEntity.status(404).body("your account is not allowed for this action");
+        }
+    }
+
 
     @PostMapping("/findAllProductByManufacturerId")
     public ResponseEntity findAllProductByManufacturerId(@Valid @RequestBody FilterSearchRequest req) {
---
+++ src/main/java/fpt/CapstoneSU24/payload/EditProductRequest.java
@@ -0,0 +1,90 @@
+package fpt.CapstoneSU24.payload;
+
+import jakarta.validation.constraints.*;
+
+import java.util.List;
+
+public class EditProductRequest {
+    @NotNull(message = "The product Id is required")
+    private int productId;
+    @NotBlank(message = "The productName is required")
+    private String productName;
+    @NotNull(message = "The certificateId is required")
+    @Digits(integer = 5, fraction = 0, message = "Invalid digit format")
+    private int categoryId;
+    @NotBlank(message = "The dimensions is required")
+    private String dimensions;
+    @NotBlank(message = "The material is required")
+    private String material;
+    @NotNull(message = "The weight is required")
+    @Min(value = 0, message = "Value must be positive")
+    private float weight;
+//    @NotBlank(message = "The unitPrice is required")
+//    private String unitPrice;
+    @NotBlank(message = "The description is required")
+    private String description;
+    @Min(value = 0, message = "Value must be positive")
+    @NotNull(message = "The warranty is required")
+    private int warranty;
+//    @NotNull(message = "The certificateId is required")
+//    @Digits(integer = 5, fraction = 0, message = "Invalid digit format")
+//    private int certificateId;
+    @NotNull(message = "The images is required")
+    private List<String> images;
+    @NotNull(message = "The avatar is required")
+    private String avatar;
+    @NotNull(message = "The file3D is required")
+    private String file3D;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+//    public String getUnitPrice() {
+//        return unitPrice;
+//    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getWarranty() {
+        return warranty;
+    }
+
+//    public int getCertificateId() {
+//        return certificateId;
+//    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public String getFile3D() {
+        return file3D;
+    }
+}
