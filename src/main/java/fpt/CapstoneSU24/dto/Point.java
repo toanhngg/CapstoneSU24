92e63707578eb7d3523f5aaa03b219ab9a9dca9b
thanhnc94 <thanhnc942@gmail.com>
2024-06-21 23:08:02 +0700

    merge to anh

---
+++ src/main/java/fpt/CapstoneSU24/dto/Point.java
@@ -0,0 +1,18 @@
+package fpt.CapstoneSU24.dto;
+
+
+// Lớp biểu diễn một điểm với tọa độ x và y
+public class Point {
+    double x;
+    double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
