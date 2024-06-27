c7809203b3a0400a8b3b4f0296f3afe1d9485088
thanhnc94 <thanhnc942@gmail.com>
2024-06-27 19:56:43 +0700

    optimize code auth

--- src/main/java/fpt/CapstoneSU24/controller/CategoryController.java
+++ src/main/java/fpt/CapstoneSU24/controller/CategoryController.java
@@ -17,6 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
@@ -43,9 +44,9 @@ public class CategoryController {
     public ResponseEntity addCategory(@Valid @RequestBody CreateCategoryRequest req) {
         int status = categoryService.addCategory(req);
         if (status == 0) {
-            return ResponseEntity.ok("create successfully");
+            return ResponseEntity.status(HttpStatus.OK).body("create successfully");
         }
-        return ResponseEntity.ok("Your account not permitted to handle this action");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Your account not permitted to handle this action");
 
     }
 
@@ -53,17 +54,17 @@ public class CategoryController {
     public ResponseEntity editCategory(@Valid @RequestBody EditCategoryRequest req) {
         int status = categoryService.editCategory(req);
         if (status == 0) {
-            return ResponseEntity.ok("edit successfully");
+            return ResponseEntity.status(HttpStatus.OK).body("edit successfully");
         }
-        return ResponseEntity.status(500).body(status == 1 ? "Can't find category by id" : "Your account not permitted to handle this action");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(status == 1 ? "Can't find category by id" : "Your account not permitted to handle this action");
     }
 
     @PostMapping("/deleteById")
     public ResponseEntity deleteById(@Valid @RequestBody IdRequest req) {
         int status = categoryService.deleteCategory(req);
         if (status == 0) {
-            return ResponseEntity.ok("delete successfully");
+            return ResponseEntity.status(HttpStatus.OK).body("delete successfully");
         }
-        return ResponseEntity.status(500).body(status == 1 ? "can't find category by id" : status == 2 ? "error delete category" : "Your account not permitted to handle this action");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(status == 1 ? "can't find category by id" : status == 2 ? "error delete category" : "Your account not permitted to handle this action");
     }
 }
