2a55aad471d883939d60ea0c12fdabafa1b4a6ec
Toanh <anhng130@gmail.com>
2024-06-28 13:51:16 +0700

    fix loi

--- src/main/java/fpt/CapstoneSU24/controller/CertificateController.java
+++ src/main/java/fpt/CapstoneSU24/controller/CertificateController.java
@@ -1,30 +1,45 @@
 package fpt.CapstoneSU24.controller;
 
+import fpt.CapstoneSU24.dto.Certificate.CertificateListDTO;
 import fpt.CapstoneSU24.dto.Certificate.CreateCertificateRequest;
+import fpt.CapstoneSU24.dto.ListCertificateDTOResponse;
+import fpt.CapstoneSU24.dto.UserProfileDTO;
+import fpt.CapstoneSU24.mapper.UserMapper;
+import fpt.CapstoneSU24.model.Certificate;
+import fpt.CapstoneSU24.model.User;
+import fpt.CapstoneSU24.payload.IdRequest;
+import fpt.CapstoneSU24.repository.CertificateRepository;
+import fpt.CapstoneSU24.repository.UserRepository;
 import fpt.CapstoneSU24.service.CertificateService;
+import fpt.CapstoneSU24.service.EpochDate;
+import fpt.CapstoneSU24.service.UserService;
+import fpt.CapstoneSU24.util.CloudinaryService;
+import jakarta.validation.Valid;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/certificate")
 public class CertificateController {
-
-    private final CertificateService certificateService;
-
     @Autowired
-    public CertificateController(CertificateService certificateService) {
-        this.certificateService = certificateService;
+    CertificateService certificateService;
+    @PostMapping("/getListCertificateByManuId")
+    public ResponseEntity<?> getListCertificateByManuId(@Valid @RequestBody IdRequest req)
+    {
+        return certificateService.getListCertificateByManuId(req);
     }
 
-    @PostMapping("/getListCertificate")
-    public ResponseEntity<?> getListCertificate(@RequestBody String req) {
-        return certificateService.getListCertificate(req);
-    }
-
-
-    @PutMapping("/createCertificate")
-    public ResponseEntity<?> createCertificate(@RequestBody CreateCertificateRequest req)
+    @GetMapping("/getListManuToVerify")
+    public ResponseEntity<?> getListManuToVerify()
     {
         return certificateService.getListManuToVerify();
     }
@@ -34,8 +49,3 @@ public class CertificateController {
         return certificateService.createCertificate(req);
     }
 }
-
-
-
-
-
