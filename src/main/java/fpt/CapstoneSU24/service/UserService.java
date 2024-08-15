800576c8f353ece31ce83c11613ad8f7adeb8450
thanhnc94 <thanhnc942@gmail.com>
2024-08-15 21:22:10 +0700

    fix cert and updateOrgName

--- src/main/java/fpt/CapstoneSU24/service/UserService.java
+++ src/main/java/fpt/CapstoneSU24/service/UserService.java
@@ -111,7 +111,7 @@ public class UserService {
                 userProfileDTO.setOrgIMG(cloudinaryService.getImageUrl(currentUser.getOrgImage()));
                 userProfileDTO.setWard(currentUser.getLocation().getWard());
                 userProfileDTO.setDistrict(currentUser.getLocation().getDistrict());
-
+                userProfileDTO.setOrgName(currentUser.getOrg_name());
                 if (userId > 0 && !isAdmin) {
                     if (checkUser == null || (checkUser.getUserId() != userId)) {
                         userProfileDTO.setEmail("");
@@ -125,6 +125,19 @@ public class UserService {
             System.out.println("loi: " + e.getMessage());
         }
         return userProfileDTO;
+    }
+
+    public ResponseEntity<String> updateOrgName(String req) {
+        JSONObject jsonReq = new JSONObject(req);
+        String description = jsonReq.has("orgName") ? jsonReq.getString("orgName") : "";
+        User user;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if ((authentication.getPrincipal() instanceof User)) {
+            user = (User) authentication.getPrincipal();
+            user.setOrg_name(description);
+            userRepository.save(user);
+        }
+        return ResponseEntity.status(200).body("orgName update success");
     }
 
     public ResponseEntity<?> viewAllManufacturer(FilterSearchManufacturerRequest req) {
@@ -219,7 +232,7 @@ public class UserService {
             B03_GetDataGridDTO.setDistrict(user.getLocation().getDistrict());
             B03_GetDataGridDTO.setWard(user.getLocation().getWard());
             B03_GetDataGridDTO.setCity(user.getLocation().getCity());
-
+            B03_GetDataGridDTO.setOrgName(user.getOrg_name());
             return B03_GetDataGridDTO;
         });
 
@@ -528,6 +541,8 @@ public class UserService {
             }
             return ResponseEntity.status(200).body("description update success");
     }
+
+
 
     public ResponseEntity<?> countRegisteredUser() {
         List<User> users = userRepository.findAll();
