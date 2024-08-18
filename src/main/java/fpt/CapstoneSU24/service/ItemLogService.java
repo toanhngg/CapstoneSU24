f37c8e99756fb935ee44712dba29fde65a2613ed
Toanh <anhng130@gmail.com>
2024-08-19 01:14:05 +0700

    merge giupp

--- src/main/java/fpt/CapstoneSU24/service/ItemLogService.java
+++ src/main/java/fpt/CapstoneSU24/service/ItemLogService.java
@@ -180,7 +180,7 @@ public class ItemLogService {
                 int check = clientService.checkOTP(dataEditDTO.getEmail().trim(), dataEditDTO.getOTP().trim(), item.getProductRecognition());
                 if (check == 6)
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Edit fail! OTP is not correct.");
-                if (check == 3 || check == 0) {
+                if (check == 3 || check == 0 || check == 8) {
                     // B2: Lưu thông tin của itemLogId đó thành một dòng itemLogId khác
                     ItemLog newItemLog = new ItemLog();
                     copyItemLogDetails(newItemLog, itemLogDetail);
